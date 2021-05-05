ac55c937c1fcb141992cdef4e659527aaea4131f
JereTardivo <49248058+JereTardivo@users.noreply.github.com>
2021-05-04 21:23:05 -0300

    Cambios JT

--- ingreso.php
+++ ingreso.php
@@ -39,8 +39,12 @@
                     <div align="center" id="formulario-act">
                         <form action="validar.php" method="post">
                             <h1>Ingreso</h1>
+                            <label style="color:white"><b>Usuario</b></label>
+                            <br>
                             <input type="text" name="usuario" id="datos-ingreso">
                             <br><br>
+                            <label style="color:white"><b>Contraseña</b></label>
+                            <br>
                             <input type="password" name="clave" id="datos-ingreso">
                             <br><br>
                             <input type="submit" name="ingresar" value="Ingresar" class="btn-get-started scrollto">
--- navegacion.php
+++ navegacion.php
@@ -23,7 +23,7 @@ session_abort();
 <link href="vendor/icofont/icofont.min.css" rel="stylesheet">
 <link href="vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
 <link href="vendor/remixicon/remixicon.css" rel="stylesheet">
-<link href="vendor/venobox/venobox.css" rel="stylesheet"> 
+<link href="vendor/venobox/venobox.css" rel="stylesheet">
 <link href="vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
 
 
@@ -35,76 +35,77 @@ session_abort();
       <ul>
         <li class="active"><a href="general.php">Home</a></li>
         <?php if ($nivel !== 'operario') { ?>
-        <li class="drop-down"><a href="#">Seguridad</a>
-          <ul>
-            <li class="drop-down"><a href="#">Temperatura</a>
-              <ul>
-                <li><a href="iTemperatura.php">Indicadores</a></li>
-                <?php if ($nivel === 'admin') { ?>
-                <li><a href="accionesTemperatura.php">Acciones</a></li>
-                <?php } ?> 
-                <li><a href="registrosTemperatura.php">Registros</a></li>
-              </ul>
-            </li>
-            <li class="drop-down"><a href="#">Movimiento</a>
-              <ul>
-                
-                <li><a href="iMovimiento.php">Indicadores</a></li>
-                <?php if ($nivel === 'admin') { ?>
-                <li><a href="accionesMovimiento.php">Acciones</a></li>
-                <?php } ?> 
-                <li><a href="registrosTemperatura.php">Registros</a></li>
-              </ul>
-            </li>
-            <li class="drop-down"><a href="#">Humedad</a>
-              <ul>
-                <li><a href="iHumedad.php">Indicadores</a></li>
-                <?php if ($nivel === 'admin') { ?>
-                <li><a href="accionesHumedad.php">Acciones</a></li>
-                <?php } ?> 
-                <li><a href="registrosHumedad.php">Registros</a></li>
-              </ul>
-            </li>
-            <li class="drop-down"><a href="#">Gas</a>
-              <ul>
-                <li><a href="iGas.php">Indicadores</a></li>
-                <?php if ($nivel === 'admin') { ?>
-                <li><a href="accionesGas.php">Acciones</a></li>
-                <?php } ?> 
-                <li><a href="registrosGas.php">Registros</a></li>
-              </ul>
-            </li>
-            <li class="drop-down"><a href="#">Sonido</a>
-              <ul>
-                <li><a href="iSonido.php">Indicadores</a></li>
-                
-              </ul>
-            </li>
-          </ul>
-        </li>
-        <li class="drop-down"><a href="#">Higiene</a>
-          <ul>
-            <li><a href="dispenser.php">Dispenser</a></li>
-          </ul>
-        </li>
-        <?php } ?> 
+          <li class="drop-down"><a href="#">Seguridad</a>
+            <ul>
+              <li class="drop-down"><a href="#">Temperatura</a>
+                <ul>
+                  <li><a href="iTemperatura.php">Indicadores</a></li>
+                  <?php if ($nivel === 'admin') { ?>
+                    <li><a href="accionesTemperatura.php">Acciones</a></li>
+                  <?php } ?>
+                  <li><a href="registrosTemperatura.php">Registros</a></li>
+                </ul>
+              </li>
+              <li class="drop-down"><a href="#">Movimiento</a>
+                <ul>
+
+                  <li><a href="iMovimiento.php">Indicadores</a></li>
+                  <?php if ($nivel === 'admin') { ?>
+                    <li><a href="accionesMovimiento.php">Acciones</a></li>
+                  <?php } ?>
+                  <li><a href="registrosTemperatura.php">Registros</a></li>
+                </ul>
+              </li>
+              <li class="drop-down"><a href="#">Humedad</a>
+                <ul>
+                  <li><a href="iHumedad.php">Indicadores</a></li>
+                  <?php if ($nivel === 'admin') { ?>
+                    <li><a href="accionesHumedad.php">Acciones</a></li>
+                  <?php } ?>
+                  <li><a href="registrosHumedad.php">Registros</a></li>
+                </ul>
+              </li>
+              <li class="drop-down"><a href="#">Gas</a>
+                <ul>
+                  <li><a href="iGas.php">Indicadores</a></li>
+                  <?php if ($nivel === 'admin') { ?>
+                    <li><a href="accionesGas.php">Acciones</a></li>
+                  <?php } ?>
+                  <li><a href="registrosGas.php">Registros</a></li>
+                </ul>
+              </li>
+              <li class="drop-down"><a href="#">Sonido</a>
+                <ul>
+                  <li><a href="iSonido.php">Indicadores</a></li>
+                </ul>
+              </li>
+              <li class="drop-down"><a href="#">Datos Generales</a>
+                <ul>
+                  <li><a href="datosregistros.php">Registros</a></li>
+                  <li><a href="grafico2.php">Grafico</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li class="drop-down"><a href="#">Higiene</a>
+            <ul>
+              <li><a href="dispenser.php">Dispenser</a></li>
+            </ul>
+          </li>
+        <?php } ?>
         <li class="drop-down"><a href="#">Usuarios</a>
           <ul>
             <li class="drop-down"><a href="#">Tablas</a>
               <ul>
                 <li><a href="datosingreso.php">Ver Ingresos</a></li>
-                <li><a href="datosregistros.php">Ver Registros</a></li>
-
               </ul>
             </li>
             <li class="drop-down"><a href="#">Graficos</a>
               <ul>
                 <li><a href="grafico1.php">Cantidad de ingresos</a></li>
-                <li><a href="grafico2.php">Cantidad de registros</a></li>
                 <li><a href="grafico3.php">Cantidad de ingresos por zona</a></li>
               </ul>
             </li>
-
           </ul>
         </li>
         <li class="drop-down get-started-btn"><a> <?php echo ucfirst($usuario) ?> </a>
